d80f1eabd9254f014a6eccaa8c43de941849817b
hyunjooooojung <hyunjoo3107@gmail.com>
2022-11-02 22:38:36 +0900

    Feat : 음원의 리뷰 수정하는 기능 #4
    1. 음원의 리뷰를 수정할 수 있는 기능 추가
    2. musics/views.py - class ReviewDetailView 추가

--- musics/urls.py
+++ musics/urls.py
@@ -3,4 +3,6 @@ from musics import views
 
 urlpatterns = [
     path('reviews/', views.ReviewView.as_view(), name='review_view'),#music_id 추가필요
+    path('reviews/put/<int:id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),#music_id 추가필요
+    
 ]
