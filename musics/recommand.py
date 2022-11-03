a5d3bd78be73933ed06df36b9ca31f16146804f7
LeeHyunji <hyunji015@gmail.com>
2022-11-03 20:16:25 +0900

    Feat: 추천시스템 협업필터링 구현 및 유사도 csv 저장 #18
    - grades.csv와 musics.csv 결합해서 평점 확인
    - 협업 필터링 통해서 유저간의 코사인 유사도 저장

---
+++ musics/recommand.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+def music_grades_merge():
+    grades = pd.read_csv('grades_data.csv')
+    musics = pd.read_csv('music_data.csv')
+    pd.set_option('display.max_columns', 10)
+    pd.set_option('display.width', 300)
+
+    # grades, musics 결합
+    music_ratings = pd.merge(grades, musics, on='music_id').sort_values(by='user_id', ascending=True)
+    title_user = music_ratings.pivot_table('grade', index='user_id', columns='music_id')
+    title_user = title_user.fillna(0)
+
+    return title_user
+
+def collaborative_filtering(title_user):
+    #코사인 유사도
+    user_based_collab = cosine_similarity(title_user, title_user)
+    user_based_collab = pd.DataFrame(user_based_collab, index=title_user.index, columns=title_user.index)
+    
+    csv_path = 'user_distance_data.csv'
+    user_based_collab.to_csv(csv_path, index=False)
+
+
+
+def func01():
+    # 1번 유저와 비슷한 유저를 내림차순으로 정렬한 후에, 상위 5개만 뽑음
+    print(user_based_collab[2].sort_values(ascending=False)[:10])
+
+    
+
+def recommand_musics(user_id):
+    # 유저와 비슷한 취향의 유저의 평점을 작성한 음원 출력
+    
+    return 0
