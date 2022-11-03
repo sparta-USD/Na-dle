ca511a5f7c63a555fc58576ee52229d64635c30f
dy <kvkvd12@gmail.com>
2022-11-03 11:26:36 +0900

    Feat: 로그아웃 기능 #8
    1. 토큰을 삭제해서 로그아웃
        - refresh값을 post하면 로그아웃
    2. users/logout/ url 생성
    3. admin 페이지 (+추가)

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from users import views
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenBlacklistView
 )
 
 urlpatterns = [
     path('signup/', views.UserView.as_view(), name='user_view'),
     path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', views.LogoutView.as_view(), name="logout"),
 ]
