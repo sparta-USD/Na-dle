06783f07ba5a9a0a5edfed44954fe16ed27aab8b
suin <parksuin55@gmail.com>
2022-11-03 15:22:45 +0900

    Feat : 음원 CRUD 생성,리뷰 CR 수정 #12,3
    1. 음원 CRUD 기능 구현
    2. 리뷰 생성 기능 수정 - music에 리뷰가 작성되도록 music_id 연결
    3. 리뷰 보여주는 기능 수정 - 음원별로 작성된 리뷰 보여주도록 역참조
       사용하여 연결(MusicDetailSerializer 추가)

--- musics/serializers.py
+++ musics/serializers.py
@@ -2,10 +2,16 @@ from rest_framework import serializers
 from .models import Music
 from musics.models import Review
 
-class ArticleSerializer(serializers.ModelSerializer):
+class MusicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Music
         fields = "__all__"
+
+class MusicCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music
+        fields = ('title','image','artist','album')
+
 
 
 class ReviewSerializer(serializers.ModelSerializer):
@@ -22,5 +28,12 @@ class ReviewSerializer(serializers.ModelSerializer):
 class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ("content","grade")
+        fields = '__all__'
 
+
+class MusicDetailSerializer(serializers.ModelSerializer):
+    reviews = ReviewSerializer(many=True)
+    
+    class Meta:
+        model = Music
+        fields = "__all__"
