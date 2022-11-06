10d4956aae21cd378aca51e98b625a91a46b2b1b
LeeHyunji <hyunji015@gmail.com>
2022-11-06 13:34:23 +0900

    Feat: 전체음원 조회 API 구현 #41 #42
    1. 기존 URL("/musics/")을 사용하고 있던 추천음악/추천유저 목록 API 분리#42
           - URL 변경 : '/musics/' -> '/musics/recommend/'
           - views.py 클래스 변경 : MusicView -> MusicRecommandView
    
    2. 음악 전체 조회API 작업
           - URL : '/musics/'
           - Requset: X
           - Response : 최신순으로 정렬된 music 전체 목록

--- musics/urls.py
+++ musics/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('<int:music_id>/reviews/', views.ReviewView.as_view(), name='review_view'),
     path('<int:music_id>/reviews/<int:review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
     path('', views.MusicView.as_view(), name='music_view'),
+    path('recommend/', views.MusicRecommandView.as_view(), name='music_recommend_view'),
     path('<int:music_id>/', views.MusicDetailView.as_view(), name='music_detail_view'),
     path('random/', views.MusicRandomView.as_view(), name='music_random_view'),
 ]
