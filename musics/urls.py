afbe2f6a94caef6f4e0978db8675fac737b1da41
LeeHyunji <hyunji015@gmail.com>
2022-11-07 16:17:17 +0900

    Refactor: Review API 리뉴얼
    1. review API 중 수정,삭제에서 music_id를 받아오지만 사용하지 않는걸로
      판단.
    2. 변경된 review API 경로
    	- 리뷰수정 : PUT 'music/review/<int:review_id'>
    	- 리뷰삭제 : Delete 'music/reveiw/<int:review_id'>
    3. ReviewCreateSerializer에서 user필드는 user.username으로 나오게 지정.

--- musics/urls.py
+++ musics/urls.py
@@ -3,7 +3,7 @@ from musics import views
 
 urlpatterns = [
     path('<int:music_id>/reviews/', views.ReviewView.as_view(), name='review_view'),
-    path('<int:music_id>/reviews/<int:review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
+    path('reviews/<int:review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
     path('', views.MusicView.as_view(), name='music_view'),
     path('recommend/', views.MusicRecommandView.as_view(), name='music_recommend_view'),
     path('<int:music_id>/', views.MusicDetailView.as_view(), name='music_detail_view'),
